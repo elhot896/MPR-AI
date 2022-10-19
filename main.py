efec130086e26397ba195ab5eb22cccccc91a040
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-10-19 13:55:17 -0400

    Added readme and main function

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import os
 import cv2
 import face_recognition
 
-files = os.listdir("./images")
-images_path = []
-encodings = []
 
-for file in files:
-    images_path.append(f"./images/{file}")
+def main():
+    imageFiles = os.listdir("./images")
+    images_path = []
+    encodings = []
 
-print("Files:", images_path)
+    for file in imageFiles:
+        images_path.append(f"./images/{file}")
 
-for image in images_path:
-    img = cv2.imread(image)
+    print("Files:", images_path)
+
+    for image in images_path:
+        img = cv2.imread(image)
+        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_econding = face_recognition.face_encodings(rgb_img)[0]
+        encodings.append(img_econding)
+
+    img = cv2.imread("unknown.jpg")
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_econding = face_recognition.face_encodings(rgb_img)[0]
-    encodings.append(img_econding)
+
+    result = face_recognition.compare_faces(encodings, img_econding)
+    print("Are they the same person?", any(result))
 
 
-img = cv2.imread("unknown.jpg")
-rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_econding = face_recognition.face_encodings(rgb_img)[0]
-
-result = face_recognition.compare_faces(encodings, img_econding)
-print("Are they the same person?", any(result))
+if __name__ == "__main__":
+    main()
