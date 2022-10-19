d5ad22b8ff1b810c8b2a1e456dea051312bb4a86
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-10-19 13:37:03 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import face_recognition
+
+files = os.listdir("./images")
+images_path = []
+encodings = []
+
+for file in files:
+    images_path.append(f"./images/{file}")
+
+print("Files:", images_path)
+
+for image in images_path:
+    img = cv2.imread(image)
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_econding = face_recognition.face_encodings(rgb_img)[0]
+    encodings.append(img_econding)
+
+
+img = cv2.imread("unknown.jpg")
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_econding = face_recognition.face_encodings(rgb_img)[0]
+
+result = face_recognition.compare_faces(encodings, img_econding)
+print("Are they the same person?", any(result))
